7460d75ff7a559473831ba92b70870a5059033ee
Shlomi Fish <shlomif@shlomifish.org>
2014-11-08 17:27:04 +0200

    Add filename_regex.

--- Dist-Zilla-Plugin-Test-TrailingSpace/lib/Dist/Zilla/Plugin/Test/TrailingSpace.pm
+++ Dist-Zilla-Plugin-Test-TrailingSpace/lib/Dist/Zilla/Plugin/Test/TrailingSpace.pm
@@ -1,14 +1,36 @@
 package Dist::Zilla::Plugin::Test::TrailingSpace;
 
-our $VERSION = '0.0.4';
+our $VERSION = '0.2.0';
 
 use 5.012;
 
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
-with 'Dist::Zilla::Role::PrereqSource';
+with qw/Dist::Zilla::Role::TextTemplate Dist::Zilla::Role::PrereqSource/;
 
 use namespace::autoclean;
+
+has filename_regex => (
+    is => 'ro',
+    isa => 'Str',
+    default => q/(?:\.(?:t|pm|pl|xs|c|h|txt|pod|PL)|README|Changes|TODO|LICENSE)\z/,
+);
+
+around add_file => sub {
+    my ($orig, $self, $file) = @_;
+
+    return $self->$orig(
+        Dist::Zilla::File::InMemory->new(
+            name => $file->name,
+            content => $self->fill_in_string($file->content,
+                {
+                    dist => \($self->zilla),
+                    filename_regex => $self->filename_regex,
+                }
+            )
+        )
+    );
+};
 
 # Register the release test prereq as a "develop requires"
 # so it will be listed in "dzil listdeps --author"
@@ -48,6 +70,21 @@ in files.
 2. From the command line
 
     $ dzil test --release
+
+=head1 DESCRIPTION
+
+This module tests adds a test for trailing whitespace in the distribution. It
+accepts the following parameters:
+
+=head2 filename_regex
+
+The regular expression for input to Test::TrailingSpace for matching the files
+to look for trailing space.
+
+Here is an example of how to override it:
+
+    [Test::TrailingSpace]
+    filename_regex = \.(?:pm|pod)\z
 
 =head1 SUBROUTINES/METHODS
 
@@ -100,7 +137,7 @@ else
 my $finder = Test::TrailingSpace->new(
    {
        root => '.',
-       filename_regex => qr/(?:\.(?:t|pm|pl|xs|c|h|txt|pod|PL)|README|Changes|TODO|LICENSE)\z/,
+       filename_regex => qr#{{ $filename_regex }}#,
    },
 );
 
